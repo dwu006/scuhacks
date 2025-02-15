379125c8a22363abebd74b8ef38e430839e2325a
dwu006 <happy4danielwu@gmail.com>
2025-02-15 14:01:46 -0800

    backend setup

---
+++ scuhacks/backend/config/db.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config(); 
+
+const uri = "";
+
+
+if (!uri) {
+  throw new Error('Missing API Key: "MONGODB_URI"');
+}
+
+const db = async () => {
+  try {
+    if (mongoose.connection.readyState === 1) {
+      return mongoose.connection.asPromise();
+    }
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
+};
+
+export default db;
